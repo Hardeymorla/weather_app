b2f007b77a52fb08677f83347c840a80437461b8
Ibrahim <ademolaraheemibrahim@gmail.com>
2025-05-13 01:06:30 +0100

    style adjusted

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ document.addEventListener("DOMContentLoaded", ()=> {
 
     async function checkWeather (city){
         const response = await fetch(apiUrl + city + `&appid=${apiKey}`);
-        if (!response.status){
+        if (response.status == 404){
             document.querySelector(".error").style.display = "block";
             document.querySelector(".weather").style.display = "none";
         }else{
